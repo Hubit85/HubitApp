10e8bb2e8312f1318d57eaddc2d3e7e9021068c1
softgenai <admin@softgen.ai>
2025-06-23 19:05:23 +0000

    Updates

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
-
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { LanguageProvider } from "@/contexts/LanguageContext";
+import MobileOptimizer from "@/components/MobileOptimizer";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <LanguageProvider>
-      <Component {...pageProps} />
+      <MobileOptimizer enablePinchZoom={true} className="min-h-screen">
+        <Component {...pageProps} />
+      </MobileOptimizer>
     </LanguageProvider>
   );
 }
